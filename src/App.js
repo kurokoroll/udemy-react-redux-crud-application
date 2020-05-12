187e1a1ff0139cff19645d5b973479327a3ba5cd
kurokoroll <regist@kurokoroll.com>
2020-05-12 13:55:56 +0900

    learn props of component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,26 @@
-import React, { Component } from 'react'; //JSXを使うために読み込む
+import React from 'react'; //JSXを使うために読み込む
 
-class App extends Component{
-  render(){
-    return(
-      /* returnで返すものは、1つのタグでないといけない */
-      /* 余計なHTMLが出力されないよう <React.Fragment> を使う */
-      <React.Fragment>
-        <label htmlFor="bar">bar</label>
-        <input type="text" onChange={() => {console.log("Change!")}} />
-      </React.Fragment>
-    )
-  }
+const App = () => {
+  const profiles = [
+    { name: "Taro", age: 10 },
+    { name: "Hanako", age: 5 },
+    { name: "Jiro" }
+  ]
+  return (
+     <div>
+       {
+         profiles.map((profile,index) => {
+           return <User name={profile.name} age={profile.age} key={index} />
+         })
+       }
+    </div>
+  )
+}
+const User = (props) => {
+  return <div>Hi, I'm {props.name}, and {props.age} years old!</div>
+}
+User.defaultProps = {
+  age: 1
 }
 
 export default App;
